9f3fb33eac1ed266aa4f5043b6c4b47c5e2e1f06
Mangpali <mangpali29@gmail,com>
2017-11-06 15:07:25 +0800

    polyline dengan klik map event

--- public/js/google-map.js
+++ public/js/google-map.js
@@ -20,6 +20,18 @@ function initialize() {
     };
     map = new google.maps.Map(document.getElementById("map"), mapProp);
 
+    poly = new google.maps.Polyline({
+        strokeColor: '#000000',
+        strokeOpacity: 1.0,
+        strokeWeight: 3
+    });
+    
+    poly.setMap(map);
+    map.addListener('click', addLatLng);
+}
+function addLatLng(event){
+    var path = poly.getPath();
+    path.push(event.latLng);
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 //Conflict code Test
